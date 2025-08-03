2d352f5065a876c5ee957cb5c828a6503df9a77a
Asif Mahmud <as.if.98914@gmail.com>
2025-08-04 00:56:09 +0600

    nothing new

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import refresh, recommend 
 from testing import jobs, user
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=["https://your-backend-domain.com"], restrict to backend's domain only
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# app.include_router(jobs.router)
+# app.include_router(user.router)
 
 app.include_router(refresh.router)
 app.include_router(recommend.router)
-app.include_router(jobs.router)
-app.include_router(user.router)
